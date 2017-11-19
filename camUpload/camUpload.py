926e83c918171945586fab84d921cb75c3d5eda0
Robert Young <bobbotron@gmail.com>
2017-11-19 12:34:28 -0500

    Updated default bucket code

--- camUpload/camUpload.py
+++ camUpload/camUpload.py
@@ -8,7 +8,7 @@ config.read('camUpload.ini')
 S3_ACCESS_KEY = config['aws']['AccessID']
 S3_SECRET_KEY = config['aws']['SecretKey']
 BUCKET_TARGET = config['aws']['BucketTarget']
-conn = tinys3.Connection(S3_ACCESS_KEY,S3_SECRET_KEY,default=BUCKET_TARGET,tls=True)
+conn = tinys3.Connection(S3_ACCESS_KEY,S3_SECRET_KEY,tls=True)
 
 camera = picamera.PiCamera()
 image_file = config['camupload']['TempFileName']
@@ -17,4 +17,4 @@ camera.capture(image_file)
 epoch_time = int(time.time())
 
 f = open(imageFile,'rb')
-conn.upload('{:d}cam.jpg'.format(epoch_time),f)
+conn.upload('{:d}cam.jpg'.format(epoch_time),f, bucket=BUCKET_TARGET)

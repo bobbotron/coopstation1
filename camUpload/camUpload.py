aa8d6d76941bf24e36db3ba86f23d35776d4c143
Robert Young <bobbotron@gmail.com>
2017-11-19 12:30:28 -0500

    Initial commit of cam upload Python 3 script

---
+++ camUpload/camUpload.py
@@ -0,0 +1,20 @@
+import picamera
+import tinys3
+import configparser
+import time
+
+config = configparser.ConfigParser()
+config.read('camUpload.ini')
+S3_ACCESS_KEY = config['aws']['AccessID']
+S3_SECRET_KEY = config['aws']['SecretKey']
+BUCKET_TARGET = config['aws']['BucketTarget']
+conn = tinys3.Connection(S3_ACCESS_KEY,S3_SECRET_KEY,default=BUCKET_TARGET,tls=True)
+
+camera = picamera.PiCamera()
+image_file = config['camupload']['TempFileName']
+camera.capture(image_file)
+
+epoch_time = int(time.time())
+
+f = open(imageFile,'rb')
+conn.upload('{:d}cam.jpg'.format(epoch_time),f)

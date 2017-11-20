ba047497f25005813dff80227b01f005aeb1d323
Robert Young <bobbotron@gmail.com>
2017-11-19 19:24:18 -0500

    Added expiry parameter

--- camUpload/camUpload.py
+++ camUpload/camUpload.py
@@ -7,19 +7,26 @@ import tinys3
 import configparser
 import time
 import os
+from datetime import timedelta
 
 config = configparser.ConfigParser()
+aws_config_key = 'aws'
+cam_upload_config_key = 'camupload'
+
 config.read('camUpload.ini')
-S3_ACCESS_KEY = config['aws']['AccessID']
-S3_SECRET_KEY = config['aws']['SecretKey']
-BUCKET_TARGET = config['aws']['BucketTarget']
-conn = tinys3.Connection(S3_ACCESS_KEY,S3_SECRET_KEY,tls=True)
+
+S3_ACCESS_KEY = config[aws_config_key]['AccessID']
+S3_SECRET_KEY = config[aws_config_key]['SecretKey']
+BUCKET_TARGET = config[aws_config_key]['BucketTarget']
+
+conn = tinys3.Connection(S3_ACCESS_KEY, S3_SECRET_KEY, tls=True)
 
 print("Initializing PiCamera")
 
 camera = picamera.PiCamera()
-image_file = config['camupload']['TempFileName']
-s3_path_prefix = config['camupload']['S3PathPrefix']
+image_file = config[cam_upload_config_key]['TempFileName']
+s3_path_prefix = config[cam_upload_config_key]['S3PathPrefix']
+days_to_expire = timedelta(days=int(config[cam_upload_config_key]['DaysToExpire']))
 
 try:
     print("Capturing image")
@@ -29,7 +36,7 @@ try:
     epoch_time = int(time.time())
 
     f = open(image_file,'rb')
-    conn.upload('{}{:d}cam.jpg'.format(s3_path_prefix, epoch_time),f, bucket=BUCKET_TARGET)
+    conn.upload('{}{:d}cam.jpg'.format(s3_path_prefix, epoch_time),f, bucket=BUCKET_TARGET, expires=t)
     print("Upload complete")
 finally:
     os.remove(image_file)
